e023bd6c7692554c076d1caabbcc78a7171ccd91
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-03-25 18:00:12 -0300

    Update exercicio057_v2.py

--- exercicio057_v2.py
+++ exercicio057_v2.py
@@ -1,6 +1,6 @@
 g = str(input('Informe o seu gênero: [M/F/NB = não binário] ')).upper().strip()[0:2]
 
 while g not in 'MFNB':
-    g = str(input('Dados inválidos. \nPor favor, informe seu gênero: [M/F/NB = não binário] ')).stip().upper()[0:2]
+    g = str(input('Dados inválidos. \nPor favor, informe seu gênero: [M/F/NB = não binário] ')).strip().upper()[0:2]
 
 print('Gênero:', g)
